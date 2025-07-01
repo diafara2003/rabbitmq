60c22752aa4cbd4387adbc7b2b0625d740b06f92
jhonnatan.uruena <jhonnatan.uruena@sinco.com.co>
2025-07-01 07:57:15 -0500

    rabbit

---
+++ Receive/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+string queueName = "hello";
+try
+{
+    var factory = new ConnectionFactory { HostName = "localhost" };
+    
+    await using var connection = await factory.CreateConnectionAsync();
+    await using var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+
+   await channel.QueueDeclareAsync(queueName, true, false, false, null);
+    await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: string.Empty);
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+    consumer.ReceivedAsync += (model, ea) =>
+    {
+        byte[] body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" [x] {message}");
+        return Task.CompletedTask;
+    };
+
+    Console.ReadLine();
+    await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+Console.ReadLine();
---
+++ Send/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+
+string queueName = "hello";
+try
+{
+    Console.WriteLine("Connecting to RabbitMQ...");
+    var factory = new ConnectionFactory { HostName = "localhost", Port = 5672, UserName = "guest", Password = "guest" };
+
+    await using var connection = await factory.CreateConnectionAsync();
+    await using var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(exchange: "logs", type: ExchangeType.Fanout);
+    
+    // await channel.QueueDeclareAsync(queueName, true, false, false, null);
+    // await channel.QueueBindAsync(queue: queueName, exchange: "logs", routingKey: string.Empty);
+
+    const string message = "Hello World!";
+    var body = System.Text.Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync(exchange: "logs", routingKey: String.Empty, body: body);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error connecting to RabbitMQ: {ex.Message}");
+}
+
+Console.WriteLine("sending message to RabbitMQ...");
+Console.ReadLine();
